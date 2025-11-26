0f81a9930dd207b202224c2e934ed7341c7e516a
Gökdeniz Kaymak <57136915+protoss70@users.noreply.github.com>
2025-11-26 16:28:37 +0100

    fix: Delete dist directory

--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import type { AirtableFieldType, DataMapping, MappingResult } from './types.js';
-export declare const mapApifyOutputToAirtableType: (value: any) => AirtableFieldType;
-export declare const getValueAtPath: (obj: any, path: string) => any;
-export declare const mapItemsToAirtableRecords: (items: any[], dataMappings: DataMapping[], uniqueId: string | undefined, uniqueIdSet: Set<string> | null) => MappingResult;
-//# sourceMappingURL=utils.d.ts.map
